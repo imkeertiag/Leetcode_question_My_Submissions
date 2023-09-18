63167adf740ed2f33b4182e19b4c5fabda9fd5c4
imkeertiag <135499447+imkeertiag@users.noreply.github.com>
2023-09-19 01:59:19 +0530

    Time: 37 ms (34.97%), Space: 21.2 MB (26.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans ;
-        unordered_map<string , vector<string> > mp ;
-        for(auto x: strs){
-            string temp = x ;
-            sort(x.begin() , x.end() ) ;
-            mp[x].push_back(temp) ;     
-        } 
-          for(auto x: mp){
-              ans.push_back(x.second) ;
-          }
-          return ans ; 
+        unordered_map<string ,vector<string> > mp ;
+        for(auto temp: strs){
+            string x = temp ;
+            sort(temp.begin() , temp.end()) ;
+            mp[temp].push_back(x) ;
+        }
+        for(auto itr: mp){
+            ans.push_back(itr.second) ;
+        }
+        return ans ;
     }
 };
