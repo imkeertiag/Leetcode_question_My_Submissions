124959886e77c9b6dff85f816bad3496c025624d
Keerti Agarwal <135499447+imkeertiag@users.noreply.github.com>
2024-06-19 23:52:50 +0530

    Time: 23 ms (29.35%), Space: 9.4 MB (93.39%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> hash;
-        int left=0 ;
-        int n= s.length() ;
-        int maxlen = 0;
-        for(int right=0;right<n;right++){
-            if(hash.count(s[right]) ==0 || hash[s[right]] < left){
-                hash[s[right]] = right ;
-                maxlen=max(maxlen , right-left+1);
-            }else{
-                left= hash[s[right]] + 1 ;
-                hash[s[right]] = right;
+      unordered_map<char , int> mp ;
+      int left = 0 ,n = s.length() ;
+      int maxlen = 0 ;
+       for(int right=0;right<n;right++){
+        mp[s[right]]++ ;
+        if(mp.size() == right-left+1){
+            maxlen=max(maxlen , right-left+1);
+            cout<<maxlen ;
+        }
+        if(mp.size() < right-left+1){
+            while(mp.size() != right-left+1){
+            mp[s[left]]-- ;
+            if(mp[s[left]] == 0)mp.erase(s[left]) ;
+            left++ ;
             }
         }
-        return maxlen ;
+       } 
+       return maxlen ; 
     }
 };
-
-
-//using unordered_set
-// int left=0 ,right=0 ,maxi=0;
-//         unordered_set<char> st;
-//         while(right<s.length()){
-//          if(st.find(s[right]) == st.end()){ // element is not present
-//          st.insert(s[right]);
-//          maxi = max(maxi , right-left+1);
-//          right++;
-//          }
-//          else{
-//             st.erase(s[left]);
-//             left++;
-//          }
-//         }
-//         return maxi;
